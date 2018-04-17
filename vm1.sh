454238345110361638879a94eb697745677d53ac
Pro <ahtyrka@gmail.com>
2018-04-18 01:23:30 +0300

    add using of source command to include config

--- vm1.sh
+++ vm1.sh
@@ -1,13 +1,57 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-EX_IF=$(cat ${SCRIPTPATH}/vm1.config | grep "^EXTERNAL_IF=" | awk -F"=" {' print $2'}| tr -d \”| tr -d \")
-V_IF=$(cat ${SCRIPTPATH}/vm1.config | grep "^INTERNAL_IF=" | awk -F"=" {' print $2'}| tr -d \”)
-V_N=$(cat ${SCRIPTPATH}/vm1.config | grep "^VLAN=" | awk -F"=" {' print $2'}| tr -d \”)
-V_IP=$(cat ${SCRIPTPATH}/vm1.config | grep "^INT_IP=" | awk -F"=" {' print $2'}| awk -F="/" {' print $1 '}| awk -F"/" {' print $1 '})
+# enable config file
+source ${SCRIPTPATH}/vm1.config
+if [ $? != 0 ]; then
+        echo "Configuration file is not persists. Please fix it"
+	exit 1
+fi
 
-VLAN_IF=${V_IF}.${V_N}
+EXTERNAL_IF=$(echo ${EXTERNAL_IF} | tr -d \”| tr -d \")
+INTERNAL_IF=$(echo ${INTERNAL_IF} | tr -d \”| tr -d \")
+EXT_IP=$(echo ${EXT_IP} | tr -d \”| tr -d \")
+#echo "EXTERNAL_IF= "${EXTERNAL_IF}
+#echo "INTERNAL_IF= "${INTERNAL_IF}
+#echo "VLAN= "${VLAN}
+#echo "EXT_IP= "${EXT_IP}
+#echo "EXT_GW= "${EXT_GW}
+#echo "INT_IP= "${INT_IP}
+#echo "VLAN_IP= "${VLAN_IP}
+#echo "APACHE_VLAN_IP= "${APACHE_VLAN_IP}
+#exit
+
+
+#EX_IF=$(cat ${SCRIPTPATH}/vm1.config | grep "^EXTERNAL_IF=" | awk -F"=" {' print $2'}| tr -d \”| tr -d \")
+#EXT_IP=$(cat ${SCRIPTPATH}/vm1.config | awk {' print $1 '} | grep "^EXT_IP=" | awk -F"=" {' print $2'}| tr -d \”| tr -d \")
+#EXT_GW=$(cat ${SCRIPTPATH}/vm1.config | grep "^EXT_GW=" | awk -F"=" {' print $2'}| tr -d \”| tr -d \")
+#V_IF=$(cat ${SCRIPTPATH}/vm1.config | grep "^INTERNAL_IF=" | awk -F"=" {' print $2'}| tr -d \”)
+#V_N=$(cat ${SCRIPTPATH}/vm1.config | grep "^VLAN=" | awk -F"=" {' print $2'}| tr -d \”)
+#V_IP=$(cat ${SCRIPTPATH}/vm1.config | grep "^INT_IP=" | awk -F"=" {' print $2'}| awk -F="/" {' print $1 '}| awk -F"/" {' print $1 '})
+
+VLAN_IF=${INTERNAL_IF}.${VLAN}
 echo "vlan if="${VLAN_IF}
+
+# test external_if
+if [[ -z $(cat /etc/network/interfaces | grep -v "^#" | grep "${EXTERNAL_IF}") ]]; then
+
+if [[ "${EXT_IP}" == "DHCP" ]]; then
+cat <<EOF >> /etc/network/interfaces
+
+auto ${EXTERNAL_IF}
+iface ${EXTERNAL_IF} inet dhcp
+EOF
+else
+cat <<EOF >> /etc/network/interfaces
+
+auto ${EXTERNAL_IF}
+iface ${EXTERNAL_IF} inet static
+address ${EXT_IP}
+gateway ${EXT_GW}
+EOF
+fi
+systemctl restart networking.service
+fi
 
 # add nameserver if it need
 TEST_NS=$(cat /etc/resolv.conf | grep "^nameserver")
@@ -30,14 +74,14 @@ cat <<EOF >> /etc/network/interfaces
 
 auto ${VLAN_IF}
 iface ${VLAN_IF} inet static
-address ${V_IP}
+address ${INT_IP}
 #netmask 255.255.255.0
-vlan_raw_device ${V_IF}
+vlan_raw_device ${INTERNAL_IF}
 EOF
 
 systemctl restart networking.service
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -I POSTROUTING -o ${EX_IF} -j MASQUERADE
+iptables -t nat -I POSTROUTING -o ${EXTERNAL_IF} -j MASQUERADE
 fi
 
 
--- vm2.sh
+++ vm2.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# enable config file
+source ${SCRIPTPATH}/vm2.config
+if [ $? != 0 ]; then
+        echo "Configuration file is not persists. Please fix it"
+        exit 1
+fi
+
+#echo "INTERNAL_IF= "${INTERNAL_IF}
+#echo "VLAN= "${VLAN}
+#echo "MANAGEMENT_IP= "${MANAGEMENT_IP}
+#echo "INT_IP= "${INT_IP}
+#echo "GW_IP= "${GW_IP}
+#echo "APACHE_VLAN_IP= "${APACHE_VLAN_IP}
+#exit
 
 V_IF=$(cat vm2.config | grep "^INTERNAL_IF=" | awk -F"=" {' print $2 '} | tr -d \")
 V_N=$(cat vm2.config | grep "^VLAN=" | awk -F"=" {' print $2 '} | tr -d \")
 V_IP=$(cat vm2.config | grep "^INT_IP=" | awk -F"=" {' print $2 '})
 V_GW=$(cat vm2.config | grep "^GW_IP=" | awk -F"=" {' print $2 '})
 
-VLAN_IF=${V_IF}.${V_N}
+VLAN_IF=${INTERNAL_IF}.${V_N}
 echo "vlan if= ${VLAN_IF}"
 
 echo install..
@@ -13,12 +29,12 @@ cat <<EOF >> /etc/network/interfaces
 
 auto ${VLAN_IF}
 iface ${VLAN_IF} inet static
-address ${V_IP}
-vlan_raw_device ${V_IF}
+address ${INT_IP}
+vlan_raw_device ${INTERNAL_IF}
 EOF
 
 systemctl restart networking.service
-route add default gw ${V_GW}
+route add default gw ${GW_IP}
 
 TEST_NS=$(cat /etc/resolv.conf | grep "^nameserver")
 if [[ -z "${TEST_NS}" ]]; then
